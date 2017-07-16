e06907ac04f7334c5621770eb45ef5699e108187
gzmud <gzimud@gmail.com>
2017-07-16 15:57:38 +0800

    Create arukascmd.sh

---
+++ arukascmd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#arukas command for api
+# reference https://arukas.io/en/documents-en/arukas-api-reference-en/#api_reference
+#app url https://app.arukas.io/
+#
+#function list
+#/api/apps
+#action:GET /app/, GET /app/{apps_id} , DELETE /apps/{app_id}
+#
+#/app-sets (#not support yet)
+#action:POST /app-sets
+#
+#/containers
+#action:GET /containers , POST /containers/{containers_id}/power , DELETE /containers/{containers_id}/power , PATCH /containers/{containers_id}
+#
+#
